13014319deaec4c3f665d93aa9215515206ec4ef
ibizugbe <abrahamibizugbe@gmail.com>
2024-07-17 12:53:06 +0100

    Style blog list.

--- src/components/BlogPostList.tsx
+++ src/components/BlogPostList.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useBlogPosts } from '../hooks/useBlogPosts';
 import CustomInput from './CustomInput';
+import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
+import { motion } from "framer-motion";
+
 
 const BlogPostList: React.FC = () => {
   const { data: posts, error, isLoading } = useBlogPosts();
@@ -32,7 +35,7 @@ const BlogPostList: React.FC = () => {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Blog Posts</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center">Blog Posts</h1>
       <form onSubmit={onSubmit} className="mb-4">
         <CustomInput
           placeholders={placeholders}
@@ -40,19 +43,64 @@ const BlogPostList: React.FC = () => {
         />
       </form>
       {filteredPosts && filteredPosts.length > 0 ? (
-        <ul>
-          {filteredPosts.map(post => (
-            <li key={post.id} className="mb-2">
-              <Link to={`/posts/${post.id}`} className="text-left w-full">
-                {post.title}
-              </Link>
-            </li>
+        <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+          {filteredPosts.map((post, i) => (
+            <BentoGridItem
+              key={post.id}
+              title={
+                <Link to={`/posts/${post.id}`} className="text-left w-full">
+                  {post.title}
+                </Link>
+              }
+              header={<Skeleton/>}
+              className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+            />
           ))}
-        </ul>
+        </BentoGrid>
       ) : (
         <div>No records found</div>
       )}
     </div>
+  );
+};
+
+const Skeleton = () => {
+  const variants = {
+    initial: {
+      width: 0,
+    },
+    animate: {
+      width: "100%",
+      transition: {
+        duration: 0.2,
+      },
+    },
+    hover: {
+      width: ["0%", "100%"],
+      transition: {
+        duration: 2,
+      },
+    },
+  };
+  const arr = new Array(6).fill(0);
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      whileHover="hover"
+      className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
+    >
+      {arr.map((_, i) => (
+        <motion.div
+          key={"skelenton-two" + i}
+          variants={variants}
+          style={{
+            maxWidth: Math.random() * (100 - 40) + 40 + "%",
+          }}
+          className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-neutral-100 dark:bg-black w-full h-4"
+        ></motion.div>
+      ))}
+    </motion.div>
   );
 };
 
