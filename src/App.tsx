2dc1b9e2126467b9e9c5283441baeada4bca5779
ibizugbe <abrahamibizugbe@gmail.com>
2024-07-17 21:31:58 +0100

    Implement navigation functionality to improve UX.

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import BlogPostList from './components/BlogPostList';
 import BlogPostDetail from './components/BlogPostDetail';
-import { useTheme } from './context/ThemeContext';
-import { BiSun, BiMoon } from 'react-icons/bi';
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
-  const { theme, toggleTheme } = useTheme();
-
   return (
-    <div className={`container mx-auto ${theme === 'dark' ? 'dark' : ''}`}>
-      <div className="flex justify-end p-4">
-        <button
-          onClick={toggleTheme}
-          className="text-xl p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-800 transition duration-300"
-        >
-          {theme === 'light' ? <BiMoon /> : <BiSun />}
-        </button>
-      </div>
+    <div className="container mx-auto">
+      <Navbar />
       <Routes>
         <Route path="/" element={<BlogPostList />} />
         <Route path="/posts/:id" element={<BlogPostDetail />} />
